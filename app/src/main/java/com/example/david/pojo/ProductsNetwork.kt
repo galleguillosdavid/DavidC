245e94fa13d68481c12fd2a996b1dc082cecb54e
galleguillosdavid <galleguillos.david@gmail.com>
2020-10-24 09:28:34 -0300

    Gradle ok

---
+++ app/src/main/java/com/example/david/pojo/ProductsNetwork.kt
@@ -0,0 +1,6 @@
+package com.example.david.pojo
+
+
+import com.google.gson.annotations.SerializedName
+
+class ProductsNetwork : ArrayList<ProductsNetworkItem>()
---
+++ app/src/main/java/com/example/david/pojo/ProductsNetworkItem.kt
@@ -0,0 +1,15 @@
+package com.example.david.pojo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductsNetworkItem(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("price")
+    val price: Int
+)
